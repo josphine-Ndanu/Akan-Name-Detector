41924c8fd77e648c92c7a138929195d9eb5173d7
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-04-09 15:58:55 +0300

    Link html to js, variables to store user input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-var maleAkan = ["kwasi", "kwado", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
+ var maleAkan = ["kwasi", "kwado", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
 
-var femaleAkan =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+ var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-var daysOfWeek=["Sunday","Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+ var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+ function getAkanName() {
+
+
+     var MM = parseFloat(document.getElementById("MM").value);
+     var DD = parseFloat(document.getElementById("DD").value);
+     var year = parseFloat(document.getElementById("year").value);
+
+     alert(MM);
+ }
+ getAkanName();
