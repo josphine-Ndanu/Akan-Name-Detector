ef21fb22da4edb5389412f68aa57406462fdb2e3
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-04-09 15:37:24 +0300

    Arrays to hold the names, and the days of the week

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var maleAkan = ["kwasi", "kwado", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
+
+var femaleAkan =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+
+var daysOfWeek=["Sunday","Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
