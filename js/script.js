ef75eb76fe3ed33ab3d1eea7999fa6843c09c7dc
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-04-10 22:41:14 +0300

    Name arrays

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
- var CC , MM, DD, YY,year;
+ var CC, MM, DD, YY, year, finalDay;
+
  function getInput() {
 
-    // var maleAkan = ["kwasi", "kwado", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
 
-    // var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-   
-    // var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-   
+
      MM = parseInt(document.getElementById("month").value);
      DD = parseInt(document.getElementById("day").value);
      year = document.getElementById("year").value;
-      CC = year.substring(0, 2);
-      YY =year.substring(2, 4);
+     CC = year.substring(0, 2);
+     YY = year.substring(2, 4);
 
-      var day = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+     var day = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
 
-    //var finalDay= (Math.floor(day));
- 
-
-
-   
-    console.log(day)
-}
+     finalDay = (Math.floor(day));
 
 
+
+
+     console.log(finalday)
+ }
+
+
+ var maleAkan = ["kwasi", "kwado", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
+
+ var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+ var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
